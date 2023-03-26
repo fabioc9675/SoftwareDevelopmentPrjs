fe761fcb2eed61e10684d7e0beb057ba3286d564
Fabian Castano <fabioc9675@gmail.com>
2023-03-26 02:47:44 -0500

    Including HTTP service in application

--- NodeMQTT/mqtt-app/src/index.js
+++ NodeMQTT/mqtt-app/src/index.js
@@ -1,10 +1,28 @@
+const express = require("express");
+const cors = require("cors");
 const mqtt = require("mqtt");
 
+const app = express(); // server
+const port = process.env.PORT || 3000;
+const server = require("http").createServer(app);
+
+// Settings
+app.set("port", port); // takes the port provided for the server or other
+
+// Middlewares
+app.use(express.json()); // every data that arrives to the server enters to this and it verifies if the data is a json
+app.use(cors());
+
+// Starting the server
+server.listen(app.get("port"), () => {
+    console.log(`Server on port ${app.get("port")}`);
+});
+
 const host = "broker.emqx.io";
-const port = "1883";
+const port_mqtt = "1883";
 const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
 
-const connectUrl = `mqtt://${host}:${port}`;
+const connectUrl = `mqtt://${host}:${port_mqtt}`;
 const client = mqtt.connect(connectUrl, {
     clientId,
     clean: true,
