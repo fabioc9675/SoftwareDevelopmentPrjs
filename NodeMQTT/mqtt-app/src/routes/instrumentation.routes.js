6e8e3764f8d03a4e2da0bc3c2f7e5acb5a7e7f92
Fabian Castano <fabioc9675@gmail.com>
2023-03-27 00:14:05 -0500

    Addition of new routes to post, update and delete data in DB

--- NodeMQTT/mqtt-app/src/routes/instrumentation.routes.js
+++ NodeMQTT/mqtt-app/src/routes/instrumentation.routes.js
@@ -12,4 +12,50 @@ router.get("/", async (req, res, next) => {
     res.json(instrumentObj);
 });
 
+// ***************************************************************
+// ****** Additional routes to get data **************************
+// ***************************************************************
+
+// ***************************************************************
+// ****** Additional routes to post data *************************
+// ***************************************************************
+
+// Posting data into database  // {'place': 'FABIAN', 'monitor': 1, 'typeDat': 'SAMPLE', 'temp_env': 24.0, 'mois_env': 46.0, 'noise_env': 430.2, 'distance': [17.0, 23.8], 'nPerson': 2}
+router.post("/", async (req, res, next) => {
+    // make a posting to the database
+    const { topic, author, varname, varvalue } = req.body; // create an object from the body of the POST request
+    const instrumentObj = new Instrument({
+        topic,
+        author,
+        varname,
+        varvalue,
+    });
+    console.log(instrumentObj);
+    // Post the data
+    await instrumentObj.save();
+    res.json({ status: "Instrument Data Recorded" });
+});
+
+// Updating data into the database
+router.put("/id/:id", async (req, res, next) => {
+    // make an update into the database
+    const { topic, author, varname, varvalue } = req.body; // create an object from the body of the POST request
+    const instrumentObj = {
+        topic,
+        author,
+        varname,
+        varvalue,
+    };
+    // Update the data
+    await Instrument.findByIdAndUpdate(req.params.id, instrumentObj);
+    res.json({ status: "Instrument Data Modified" });
+});
+
+// Deleting data in the database
+router.delete("/id/:id", async (req, res) => {
+    // Make a delete of data into the database
+    await Instrument.findByIdAndRemove(req.params.id);
+    res.json({ status: "Instrument Data Deleted" });
+});
+
 module.exports = router;
