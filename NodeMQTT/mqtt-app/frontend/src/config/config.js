f5d8ff42f7047c4187798f2a19edf59c5898f024
Fabian Castano <fabioc9675@gmail.com>
2023-04-04 10:06:20 -0500

    change in public direction for axios instance

--- NodeMQTT/mqtt-app/frontend/src/config/config.js
+++ NodeMQTT/mqtt-app/frontend/src/config/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://iotudeab4a-fabioc9675.b4a.run/",
+    baseURL: "https://iotudeab4a1-fabioc9675.b4a.run/",
     // baseURL: "http://localhost:5000",
 });
