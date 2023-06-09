22f4436fa53db0c93a55d09dbe1f4f5c00d06901
fabioc9675 <fabian.castano@udea.edu.co>
2023-06-08 22:35:41 -0500

    deploy of frontend

--- FastAPI_deploy_prj/app.py
+++ FastAPI_deploy_prj/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 # esta es la ruta creada en el archivo user de rutas
 from routes.monitor import monitor
 from docs import tags_metadata
@@ -10,6 +11,10 @@ app = FastAPI(
     version="0.0.1",
     openapi_tags=tags_metadata
 )
+
+# despliegue de frontend
+app.mount("/", StaticFiles(directory="frontend/build",
+          html=True), name="frontend")
 
 app.add_middleware(
     CORSMiddleware,
