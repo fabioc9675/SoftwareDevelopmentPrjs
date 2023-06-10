d61229af3bd196257d5dba64bdc37f7f36875bfa
fabioc9675 <fabian.castano@udea.edu.co>
2023-06-10 11:02:19 -0500

    change in api url information

--- FastAPI_deploy_prj/frontend/src/config/config.js
+++ FastAPI_deploy_prj/frontend/src/config/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://fastapiexample-fabioc9675.b4a.run/",
+  baseURL: "https://fastapiexample-marusugag.b4a.run/",
 });
