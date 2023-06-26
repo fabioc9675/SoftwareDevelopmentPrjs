b09bd5f143de8b14cfbb6ae8c780e26f5958a6b5
fabioc9675 <fabian.castano@udea.edu.co>
2023-06-25 23:08:52 -0500

    Creation of function to predict images from Django

--- DjangoTensorFlow/core/urls.py
+++ DjangoTensorFlow/core/urls.py
@@ -14,9 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from .views import index
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name="index")
 ]
+
+if settings.DEBUG:
+    # setting this to view media files from admin panel
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
